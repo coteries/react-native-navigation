a3405dd3b3f61b05056d911719a5f5ce877999d0
Manuel Spuhler <nicedexter@users.noreply.github.com>
2017-05-08 14:55:32 +0200

    Update RCCTabBarController.m

--- ios/RCCTabBarController.m
+++ ios/RCCTabBarController.m
@@ -193,6 +193,12 @@
   // replace the tabs
   self.viewControllers = viewControllers;
   
+  // set selected tab index
+  NSUInteger *selectedIndex = [props[@"selectedIndex"] unsignedIntegerValue];
+  if (selectedIndex && selectedIndex < viewControllers.count) {
+    [self setSelectedIndex:selectedIndex];
+  }
+  
   [self setRotation:props];
   
   return self;
